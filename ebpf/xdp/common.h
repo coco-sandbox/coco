7b614e138f5722b9869f3b6bc4f9ff3de4cd333f
nonomnonom <hi@nonom.xyz>
2026-04-26 19:32:41 +0700

    Remove unused eBPF loader, map, and object files along with associated Makefiles. This cleanup eliminates redundant code and simplifies the project structure.

--- ebpf/xdp/common.h
+++ ebpf/xdp/common.h
@@ -2,10 +2,30 @@
 #define _COCO_XDP_COMMON_H
 
 #include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
 #include <bpf/bpf_helpers.h>
 
 #define XDP_PASS 1
 #define XDP_DROP 2
 #define XDP_TX 3
+
+#ifndef IPPROTO_ICMP
+#define IPPROTO_ICMP   1
+#endif
+#ifndef IPPROTO_TCP
+#define IPPROTO_TCP    6
+#endif
+#ifndef IPPROTO_UDP
+#define IPPROTO_UDP    17
+#endif
+
+static __always_inline __u16 ip_fast_csum(const struct iphdr *iph, __u32 ihl) {
+    __u32 sum = 0;
+    __u16 *ptr = (__u16 *)iph;
+    for (__u32 i = 0; i < ihl; i++)
+        sum += *ptr++;
+    return ~(__u16)(sum + (sum >> 16));
+}
 
 #endif
